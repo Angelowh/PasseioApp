fa4054735dc7fb59b0d97d05d41b9bad79a9124b
Pedro <pedroroangelo2002@gmail.com>
2025-09-25 13:57:43 -0300

    criando logica para o form

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  categoriaForm: FormGroup;
 
+  constructor() {
+    this.categoriaForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      descricao: new FormControl('', [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  salvar(){
+    console.log(this.categoriaForm.value);
+    console.log('IsValid:' + this.categoriaForm.valid);
+  }
 }
