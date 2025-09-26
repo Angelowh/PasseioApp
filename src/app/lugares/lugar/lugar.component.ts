36910f93ed73d4b5f62a21f0818ccccb4ee0ce44
Pedro <pedroroangelo2002@gmail.com>
2025-09-26 10:47:42 -0300

    criando form para lugares

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
 
 @Component({
   selector: 'app-lugar',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './lugar.component.scss'
 })
 export class LugarComponent {
+  lugarForm: FormGroup
+  categorias: Categoria[] =[]
 
+  constructor() {
+    this.lugarForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required)
+    })
+  }
+
+  salvar(){
+    console.log(this.lugarForm.value)
+  }
 }
