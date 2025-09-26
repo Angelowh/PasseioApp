a972baf78d2313a00bcc8978c136eaecbc6899fc
Pedro <pedroroangelo2002@gmail.com>
2025-09-26 10:00:58 -0300

    criando variaveis de ambiente

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -2,19 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Categoria } from './categoria';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
-
+  baseUrl = environment.apiUrl;
+  
   constructor(private http: HttpClient) { }
 
   salvar(categoria: Categoria) : Observable<Categoria> {
-    return this.http.post<Categoria>('http://localhost:3000/categorias', categoria);
+    return this.http.post<Categoria>(`${this.baseUrl}/categorias`, categoria);
   }
 
   obterTodos() : Observable<Categoria[]> {
-    return this.http.get<Categoria[]>('http://localhost:3000/categorias');
+    return this.http.get<Categoria[]>(`${this.baseUrl}/categorias`);
   }
 }
