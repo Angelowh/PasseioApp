ff70d70d96fc607bb8557aeaee4e8084b22f4c5d
Pedro <pedroroangelo2002@gmail.com>
2025-09-29 11:40:43 -0300

    tornando titulo e subtitulo dinamicos

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LayoutProps } from './layoutProps';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  layoutProps: LayoutProps = { titulo: '', subTitulo: '' };
 
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(() => this.route.firstChild !== null),
+      map(() => this.setLayoutProps())
+    )
+    .subscribe((layoutProps: LayoutProps) => {
+      this.layoutProps = layoutProps;
+    });
+  }
+
+  setLayoutProps(): LayoutProps {
+    let rotaFilha = this.route.firstChild;
+
+    while (rotaFilha?.firstChild) {
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 }
--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -9,15 +9,21 @@ const routes: Routes = [
     children: [
       {
         path: 'categorias',
-        loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule)
+        loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule),
+        pathMatch: 'full',
+        data: { titulo: 'Categorias', subTitulo: 'Cadastre uma nova categoria' }
       },
       {
         path: 'lugares',
-        loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule)
+        loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule),
+        pathMatch: 'full',
+        data: { titulo: 'Lugares', subTitulo: 'Cadastre um novo lugar' }
       },
       {
         path: 'galeria',
-        loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule)
+        loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule),
+        pathMatch: 'full',
+        data: { titulo: 'Galeria', subTitulo: 'Listagem de lugares' }
       }
     ]
   }
